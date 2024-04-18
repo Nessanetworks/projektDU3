e8f0ef7e1672334380fea80e39426abe0c953311
julialundquist1 <egna_hemgirl@hotmail.com>
2024-04-18 14:22:24 +0200

    css changes

--- components/logIn/logIn.js
+++ components/logIn/logIn.js
@@ -2,5 +2,15 @@ function renderLogInPopUp (parentID) {
     let divDom = document.createElement("div")
     divDom.id = "popUpContainer";
     document.getElementById(parentID).append(divDom);
-
+    divDom.innerHTML = `
+    <div id="popUpBox">
+        <div id="popUpIcon"></div>
+        <p>LOGGA IN</p>
+        <input id="userNameLogIn" type="text" placeholder ="Användarnamn">
+        <input id="passwordLogIn" type="password" placeholder ="Lösenord">
+        <button id="logInButton">LOGGA IN</button>
+        <p>Har du inget konto?</p>
+        <p>Skapa konto här</p>
+    </div>
+    `;
 }
