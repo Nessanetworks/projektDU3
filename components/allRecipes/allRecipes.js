4bbfe14e473ba5573e8bc65163c6bd78fcdb7ae7
julialundquist1 <egna_hemgirl@hotmail.com>
2024-04-18 12:51:14 +0200

    css update

--- components/allRecipes/allRecipes.js
+++ components/allRecipes/allRecipes.js
@@ -5,11 +5,17 @@ function renderAllRecipesContainer(parentID) {
     document.getElementById(parentID).append(recipesPage);
     recipesPage.innerHTML = `
     <div id="navigationContainer">
-        <div id="allRecipes">ALLA RECEPT</div>
-        <div id="sortRecipes">SORTERA RECEPT</div>
-        <div id="searchRecipes"></div>
-        <div id="navigationIcon"></div>
-        <div id="logInOrUserName">LOGGA IN</div>
+        <div id="navigationLeft">
+            <div id="allRecipes">ALLA RECEPT</div>
+            <div id="sortRecipes">SORTERA RECEPT</div>
+            <div id="searchRecipes"></div>
+        </div>
+        <div id="navigationMiddle">
+            <div id="navigationIcon"></div>
+        </div>
+        <div id="navigationRight">
+            <div id="logInOrUserName">LOGGA IN</div>
+        </div>
     </div>
     <div id="allRecipesContainer"></div>
     `;
