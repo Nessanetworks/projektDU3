e70813c74eb714065356561f86cf5882eb08928a
RmebbMach <94225083+RmebbMach@users.noreply.github.com>
2024-04-18 19:01:50 +0200

    created navigation component

--- components/allRecipes/allRecipes.js
+++ components/allRecipes/allRecipes.js
@@ -3,20 +3,10 @@ function renderAllRecipesContainer(parentID) {
     let recipesPage = document.createElement("div");
     recipesPage.id = "allRecipesPage";
     document.getElementById(parentID).append(recipesPage);
-    recipesPage.innerHTML = `
-    <div id="navigationContainer">
-        <div id="navigationLeft">
-            <div id="allRecipes">ALLA RECEPT</div>
-            <div id="sortRecipes">SORTERA RECEPT</div>
-            <div id="searchRecipes"></div>
-        </div>
-        <div id="navigationMiddle">
-            <div id="navigationIcon"></div>
-        </div>
-        <div id="navigationRight">
-            <div id="logInOrUserName">LOGGA IN</div>
-        </div>
-    </div>
+
+    navigation("allRecipesPage");
+
+    recipesPage.innerHTML += `
     <div id="allRecipesContainer"></div>
     `;
 
@@ -24,7 +14,9 @@ function renderAllRecipesContainer(parentID) {
         renderLogInPopUp("wrapper");
     });
 
-    for(let i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
         renderRecipesList("allRecipesContainer");
     }
 }
+
+
--- components/recipePage/recipePage.js
+++ components/recipePage/recipePage.js
@@ -1,32 +1,39 @@
 "use strict";
 
 function recipePage(parentID) {
-    document.getElementById(parentID).innerHTML = `
-        <div id="recipePage">
-            <img id="recipeImg" src="./media/images/chilibowl.webp" alt="">
-            <div id="rightContainer">
-                <h2>Chili corn carne</h2>
-                <stars>
-                    <p>++++++</p>
-                <stars>
-                <h3>Lägg till betyg</h3>
-                <div id="timer">
-                    <img id="timer" src="./media/icons/timer.png alt="">
-                    <p> min</p>
-                </div>
-                <div id="ingredients:">
-                    <h3>Ingredienser</h3>
-                    <ul>
+    document.getElementById(parentID).innerHTML = "";
+    let recipePage = document.createElement("div");
+    recipePage.id = "recipePage";
+    document.getElementById(parentID).appendChild(recipePage);
+
+    navigation("recipePage");
+
+    recipePage.innerHTML += `
+    <div id="recipeContainer">
+        <img id="recipeImg" src="./media/images/chilibowl.webp" alt="">
+        <div id="rightContainer">
+            <h2>Chili corn carne</h2>
+            <stars>
+                <p>++++++</p>
+            <stars>
+            <h3>Lägg till betyg</h3>
+            <div id="timer">
+                <img id="timer" src="./media/icons/timer.png alt="">
+                <p> min</p>
+            </div>
+            <div id="ingredients:">
+                <h3>Ingredienser</h3>
+                <ul>
                         
-                    </ul>
-                </div>
-                <div>
-                    <h3>Gör såhär:</h3>
-                    <ol>
-                    </ol>
-                </div>
+                </ul>
+            </div>
+            <div>
+                <h3>Gör såhär:</h3>
+                <ol>
+                </ol>
             </div>
         </div>
+    </div>
     `
     console.log("Funkar");
 }
