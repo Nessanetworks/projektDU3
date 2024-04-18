fee116dc800bcd9085b1bd02f598ec6717b61421
RmebbMach <94225083+RmebbMach@users.noreply.github.com>
2024-04-18 14:31:51 +0200

    created recipePage function

---
+++ components/recipePage/recipePage.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function recipePage(parentID) {
+    let recipePage = document.createElement("div");
+    recipePage.id = "recipePage";
+
+    document.getElementById(parentID).appendChild(recipePage);
+
+    recipePage.innerHTML = `
+        <img src="" alt="">
+        <div id="recipeText">
+            <h2><h2>
+            <stars><stars>
+            <p>Lägg till betyg</p>
+            <div id="timer">
+                <img src="" alt="">
+            </div>
+            <div id="ingredients">
+                <h3>Ingredienser</h3>
+                <ul>
+                
+                </ul>
+            </div>
+            <div>
+                <h3>Gör såhär:</h3>
+                <ol>
+                </ol>
+            </div>
+        <div>
+    `
+
+    console.log("Funkar");
+}
+
