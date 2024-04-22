c5264b6e44697572066d025242f4a9e47eea08e6
Alba Girlinger <albagirlinger@hotmail.com>
2024-04-22 16:25:12 +0200

    search and filter rending

--- components/sort/sort.js
+++ components/sort/sort.js
@@ -1,42 +1,60 @@
-function renderIngredientSearch(parentID) {
-    let divDom = document.createElement("div");
-    divDom.id = "divSearchIngredient";
-    document.getElementById(parentID).append(divDom);
+let searchPopUpVisible = false;
 
-    divDom.innerHTML = `
-    <input type="text" id="inputSearchIngredient" class="hide" placeholder="SÖK EFTER INGREDIENS...">
-    `;
+function renderIngredientSearch(parentID) {
+    if (!searchPopUpVisible) {
+        let divDom = document.createElement("div");
+        divDom.id = "divSearchIngredient";
+        document.getElementById(parentID).append(divDom);
+
+        divDom.innerHTML = `
+        <input type="text" id="inputSearchIngredient" placeholder="SÖK EFTER INGREDIENS...">
+        `;
+        searchPopUpVisible = true;
+    } else {
+        searchPopUpVisible = false;
+        document.getElementById("divSearchIngredient").remove();
+    }
+
 }
 
-function renderIngredientSort(parentID) {
-    let divDom = document.createElement("div");
-    divDom.id = "divSort";
-    document.getElementById(parentID).append(divDom);
+let sortPopUpVisible = false;
 
-    divDom.innerHTML = `
-    <div id="highestRanking">
-        <ul>
-            <li>HÖGST BETYG</li>
-        </ul>
-        <input type="checkbox" id="checkHighestRanking">
-    </div>
-    <div id="lowestRanking">
-        <ul>
-            <li>LÄGST BETYG</li>
-        </ul>
-        <input type="checkbox" id="checkLowestRanking">
-    </div>
-    <div id="longestTime">
-        <ul>
-            <li>LÄNGST TILLAGNINGSTID</li>
-        </ul>
-        <input type="checkbox" id="checkLongestTime">
-    </div>
-    <div id="shortestTime">
-        <ul>
-            <li>KORTAST TILLAGNINGSTID</li>
-        </ul>
-        <input type="checkbox" id="checkShortestTime">
-    </div>
-    `;
+function renderIngredientSort(parentID) {
+    if (!sortPopUpVisible) {
+        let divDom = document.createElement("div");
+        divDom.id = "divSort";
+        document.getElementById(parentID).append(divDom);
+
+        divDom.innerHTML = `
+        <div id="highestRanking">
+            <ul>
+                <li>HÖGST BETYG</li>
+            </ul>
+            <input type="checkbox" id="checkHighestRanking">
+        </div>
+        <div id="lowestRanking">
+            <ul>
+                <li>LÄGST BETYG</li>
+            </ul>
+            <input type="checkbox" id="checkLowestRanking">
+        </div>
+        <div id="longestTime">
+            <ul>
+                <li>LÄNGST TILLAGNINGSTID</li>
+            </ul>
+            <input type="checkbox" id="checkLongestTime">
+        </div>
+        <div id="shortestTime">
+            <ul>
+                <li>KORTAST TILLAGNINGSTID</li>
+            </ul>
+            <input type="checkbox" id="checkShortestTime">
+        </div>
+        `;
+        sortPopUpVisible = true;
+    } else {
+        sortPopUpVisible = false;
+        document.getElementById("divSort").remove();
+    }
+
 }
