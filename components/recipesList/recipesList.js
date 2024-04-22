407b1cc25d48f1b1c29a167926a5424bef6c3a07
julialundquist1 <egna_hemgirl@hotmail.com>
2024-04-22 13:02:18 +0200

    styled recipesList

--- components/recipesList/recipesList.js
+++ components/recipesList/recipesList.js
@@ -1,11 +1,25 @@
-function renderRecipesList(parentID) {
+function renderRecipesList(parentID, data) {
     let divDom = document.createElement("div");
     divDom.classList.add("recipesInstance");
     document.getElementById(parentID).append(divDom);
 
-    divDom.addEventListener("click", () => {
-        recipePage("wrapper")
+    divDom.innerHTML = `
+    <div id="recipesListContainer">
+        <div id="recipesListImageContainer">
+            <img class="recipeImage" src=${data.picture}>
+        </div>
+        <div id="recipesTextContainer">
+            <p id="recipeListName">${data.name.toUpperCase()}</p>
+            <div id="recipeTimeContainer">
+                <div id="timerImage"></div>
+                <p id="recipeListTime">${data.time} min</p>
+            </div>
+        </div>
+    </div>
+    `;
 
+    divDom.addEventListener("click", () => {
+        recipePage("wrapper");
     })
 
 
