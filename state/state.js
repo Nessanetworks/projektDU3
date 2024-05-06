7318421d1372373ac370053cb212b188e030f331
RmebbMach <94225083+RmebbMach@users.noreply.github.com>
2024-05-06 22:47:33 +0200

    patch api

--- state/state.js
+++ state/state.js
@@ -27,33 +27,33 @@ let State = {
             newRecipePopUp("profilePageContainer");
         }
     },
-    patch: async function (data) {
-        data.token = token;
-        let options = {
-            method: "PATCH",
-            headers: { "Content-type": "application/json" },
-            body: JSON.stringify(data)
-        };
-        console.log(options)
+    // patch: async function (data) {
+    //     data.token = token;
+    //     let options = {
+    //         method: "PATCH",
+    //         headers: { "Content-type": "application/json" },
+    //         body: JSON.stringify(data)
+    //     };
+    //     console.log(options)
 
-        try {
-            const response = await fetcher(`/api/recipes.php`, options);
-            console.log(response)
+    //     try {
+    //         const response = await fetcher(`/api/recipes.php`, options);
+    //         console.log(response)
 
-            if (response.ok) {
-                let resource = await response.json();
-                console.log(resource);
+    //         if (response.ok) {
+    //             let resource = await response.json();
+    //             console.log(resource);
 
-            }
-        } catch (error) {
-            console.log(error);
-        }
-        // console.log(response)
-        // for (let i = 0; i < STATE.users.length; i++) {
+    //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    //     // console.log(response)
+    //     // for (let i = 0; i < STATE.users.length; i++) {
 
-        //     console.log(STATE.users[i]["favorites"])
-        // }
-    }
+    //     //     console.log(STATE.users[i]["favorites"])
+    //     // }
+    // }
 }
 
 async function fetcher(request, options) {
