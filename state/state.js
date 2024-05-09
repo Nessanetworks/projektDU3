b432710ad6cc6594ec80897ad2c82aeac7295c8e
Nessanetworks <Nessanetworks@users.noreply.github.com>
2024-05-09 16:28:02 +0200

    Update state.js

--- state/state.js
+++ state/state.js
@@ -71,6 +71,44 @@ let State = {
             }
         } else {
             console.error("Failed to patch favorites");
+<<<<<<< Updated upstream
+=======
+            data.element.classList.toggle('filled', !data.filled);
+            data.element.style.color = !data.filled ? 'red' : 'rgb(117, 134, 80)';
+            data.element.innerHTML = !data.filled ? '&#x2764;' : '&#x2764;';
+
+            let divParent = document.getElementById("wrapper");
+            let popUpNotLoggedIn = document.createElement("div");
+            popUpNotLoggedIn.innerHTML = "";
+
+            divParent.appendChild(popUpNotLoggedIn);
+            popUpNotLoggedIn.innerHTML = `
+            <p id="needToLogIn">Du måste vara inloggad för att kunna använda den här funktionen!</p>
+            <div id="returnLogInBox">
+                <button id="goToLogInAgain">LOGGA IN</button>
+            </div>
+            <button class="exitButton2">X</button>
+            `;
+            popUpNotLoggedIn.style.position = 'fixed';
+            popUpNotLoggedIn.style.fontSize = "20px";
+            popUpNotLoggedIn.style.left = '34.5vw';
+            popUpNotLoggedIn.style.top = '50%';
+            popUpNotLoggedIn.style.width = "400px";
+            popUpNotLoggedIn.style.height = "200px";
+            popUpNotLoggedIn.style.padding = "25px";
+            popUpNotLoggedIn.style.backgroundColor = "rgba(248, 249, 248, 0.95)";
+            popUpNotLoggedIn.style.border = "1px solid black";
+            popUpNotLoggedIn.style.zIndex = '1000';
+            popUpNotLoggedIn.style.display = 'block';
+            popUpNotLoggedIn.querySelector(".exitButton2").addEventListener('click', function () {
+                popUpNotLoggedIn.remove();
+            });
+
+            document.getElementById("goToLogInAgain").addEventListener("click", function () {
+                renderLogInPopUp("wrapper");
+                popUpNotLoggedIn.remove();
+            })
+>>>>>>> Stashed changes
         }
 
     }
